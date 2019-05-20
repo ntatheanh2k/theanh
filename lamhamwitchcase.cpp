0865dd294b267fe1b8a7ecad869cebc5a0538233
ntatheanh2k <ntatheanh2k@users.noreply.github.com>
2019-05-20 08:37:34 +0700

    Create lamhamwitchcase.cpp

---
+++ lamhamwitchcase.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("nhap so a=\n");
+	scanf("%d",&a);
+	
+	switch(a){
+		case 2: printf("hom nay la thu 2\n");break;
+		case 3: printf("hom nay la thu 3\n");break;
+		case 4: printf("hom nay la thu 4\n");break;
+		case 5: printf("hom nay la thu 5\n");break;
+		case 6: printf("hom nay la thu 6\n");break;
+		case 7: printf("hom nay la thu 7\n");break;
+		case 8: printf("hom nay la chu nhat\n");break;
+		default : printf("khong phai ngay trong tuan\n");break;
+		
+		}
+		}
+		
+
