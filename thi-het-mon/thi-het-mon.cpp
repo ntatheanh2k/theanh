4136c3c0950d0cf3ed669d44f63a70b8603137fe
ntatheanh2k <ntatheanh2k@users.noreply.github.com>
2019-05-29 16:32:56 +0700

    Create thi-het-mon.cpp

---
+++ thi-het-mon/thi-het-mon.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+int a;
+{
+	printf ("nhap mot so tu ban phim");
+	scanf ("%d",&a);
+	for (int i=1;i<a ;i++){
+	
+	if(a>1,a/2==0){
+		printf ("a la so nguyen to");
+	}else{
+		printf (" a khong phai la so nguyen to");
+	}
+	return 0;
+	
+	}
+}
+}
+
+
